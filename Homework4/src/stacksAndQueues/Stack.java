df44d699748fbeaf82a2f6dd8933775253da7464
kurt <kurt@kurt-ThinkPad-T440>
2017-03-30 12:02:09 -0400

    Added comments to stack and updated push

--- Homework4/src/stacksAndQueues/Stack.java
+++ Homework4/src/stacksAndQueues/Stack.java
@@ -1,49 +1,72 @@
 package stacksAndQueues;
 
 public class Stack {
-	
+	// Variables
 	private Node top = null;
-	
-	public boolean isEmpty(){
+
+	/**
+	 * Tests if stack is empty
+	 * 
+	 * @return true if stack is empty
+	 */
+	public boolean isEmpty() {
 		return this.top == null;
 	}
-	
-	public Node peek(){
+
+	/**
+	 * Looks at top node
+	 * 
+	 * @return top node
+	 */
+	public Node peek() {
 		return this.top;
 	}
-	
-	public void push(String input){
+
+	/**
+	 * adds to stack
+	 * 
+	 * @param input
+	 */
+	public void push(String input) {
 		Node newNode = new Node(input);
-		
-		newNode.setNext(this.top);
-		this.top = newNode;
-		
+
+		if (isEmpty()) {
+			this.top = newNode;
+		} else {
+			newNode.setNext(this.top);
+			this.top = newNode;
+		}
 	}
-	
-	public Node pop(){
-		
-		if(isEmpty()){
+	/**
+	 * Shows and removes top node
+	 * @return top node
+	 */
+	public Node pop() {
+
+		if (isEmpty()) {
 			return null;
 		}
-		
-		else{
+
+		else {
 			Node temp = top;
 			this.top = top.getNext();
 			return temp;
 		}
-		
+
 	}
-	
-	public void print(){
-		
-		if(isEmpty()){
+	/**
+	 * Prints stack
+	 */
+	public void print() {
+
+		if (isEmpty()) {
 			System.out.println("Stack is Empty.");
 		}
-		
-		else{
+
+		else {
 			Node temp = this.top;
-			
-			while(temp != null){
+
+			while (temp != null) {
 				System.out.println(temp.getData());
 				temp = temp.getNext();
 			}
